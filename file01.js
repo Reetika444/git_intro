1f8fbf290c10145ced728792ef6f5043a5b34b1c
unknown <reetikadahiya444@gmail.com>
2020-10-06 15:29:56 +0530

    add 'nice to have' msg
    Signed-off-by: unknown <reetikadahiya444@gmail.com>

--- file01.js
+++ file01.js
@@ -2,5 +2,7 @@ function greet(name){
     console.log('knock knock konck '+name)
     console.log('knock knock konck '+name)
     console.log('knock knock konck '+name)
+    console.log("Nice to have you here!")
 }
 greet('Reetika')
+
